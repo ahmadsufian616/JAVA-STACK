8a934f5a576b6ce5f85f37ae3e9d831d03ee05c2
ahmadsufian616 <ahmad11213731@gmail.com>
2021-06-09 11:19:01 +0300

    test

---
+++ FirstJava.java
@@ -0,0 +1,11 @@
+
+
+
+public class FirstJava{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My name is ahmad ");
+        System.out.println("i am 27 years old");
+        System.out.println("my hometown is nables");
+    }
+}
